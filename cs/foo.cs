26ec36a12b841d06e64763657c298f21a37f0132
Nathan Chappell <nathan.s.chappell@gmail.com>
2021-12-19 08:32:45 +0100

    tmp

--- cs/foo.cs
+++ cs/foo.cs
@@ -4,12 +4,34 @@ public class A
 {
 }
 
+public interface IE
+{
+    event System.EventHandler ie;
+}
+
+public class A1 : System.Attribute
+{
+}
+
+public class A2 : System.Attribute
+{
+    public string foo;
+    public int x;
+    public A2(string foo, int x)
+    {
+        this.foo = foo;
+        this.x = x;
+    }
+}
+
+[A1]
 public static class Program
 {
+    [A2("meow", 17)]
     public static void Main(string[] args)
     {
         dynamic x = new A();
         x.foo = "bar";
-        Console.WriteLine("x: {0}, x.foo: {1}".Format(x,x.foo));
+        Console.WriteLine(String.Format("x: {0}, x.foo: {1}",x,x.foo));
     }
 }
